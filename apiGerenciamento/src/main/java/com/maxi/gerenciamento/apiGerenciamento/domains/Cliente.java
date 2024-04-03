0dfc3f0c5f2439eced7d42252f757e45e5acbf60
maxiamikel <maxloversist@gmail.com>
2024-04-03 06:02:57 -0300

    Adicionar constructor para ClienteDTO

--- apiGerenciamento/src/main/java/com/maxi/gerenciamento/apiGerenciamento/domains/Cliente.java
+++ apiGerenciamento/src/main/java/com/maxi/gerenciamento/apiGerenciamento/domains/Cliente.java
@@ -2,9 +2,11 @@ package com.maxi.gerenciamento.apiGerenciamento.domains;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.maxi.gerenciamento.apiGerenciamento.domains.enums.Perfil;
+import com.maxi.gerenciamento.apiGerenciamento.dtos.ClienteDTO;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -27,6 +29,18 @@ public class Cliente extends Pessoa{
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Cliente(ClienteDTO obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfils = obj.getPerfils().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCadastro = obj.getDataCadastro();
         addPerfil(Perfil.CLIENTE);
     }
 
